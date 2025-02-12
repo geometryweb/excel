54168cc5964418d263423401b5ed927f5f2c167f
geometryweb <geometryweb007@gmail.com>
2025-02-12 22:20:10 +0900

    Add files via upload

--- excel_sample.py
+++ excel_sample.py
@@ -3,9 +3,11 @@ import streamlit as st
 
 st.markdown("""
             <style>
+            
             /*.e14lo1l1.st-emotion-cache-jl9d4a.ex0cdmw0 */
             /*.st-emotion-cache-1ba8tjn.e1d5ycv523 */
-            .e14lo1l1.st-emotion-cache-jl9d4a.ex0cdmw0 {
+            /*.e14lo1l1.st-emotion-cache-jl9d4a.ex0cdmw0 */
+            .e1fexwmo1.st-emotion-cache-jl9d4a.ex0cdmw0 {
                 display: none;
                 visibility: hidden;
             }   
